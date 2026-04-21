8b16dfc1dcaa23d04b88dfdfbf6c697bd09f4cdd
ARUNASR09 <aruna.singireddy@gmail.com>
2026-04-21 14:24:55 -0400

    Add files via upload

---
+++ javascript/pysample.py
@@ -0,0 +1,19 @@
+x = {'key' : 'value','lines' : 25}
+print(x)
+y =['bob','alice']
+print(y)
+
+a = 17 
+if a < 20 :
+    print("Below 20")
+else : 
+    print("20 or higher")
+
+
+for i in range(5):
+    print("value of i :", i)
+
+
+print('/a/b/c'.split('/'))
+c= 'hello' + '\n' + 'world'
+print(c)
---
+++ javascript/sample.js
@@ -0,0 +1,26 @@
+//use node -v to check it node version and node sample.js to execute code.
+x = {key : 'value',lines : 25}
+console.log(x)
+
+y =['bob','alice']
+console.log(y)
+
+a = 17 
+if( a < 20)
+{
+    console.log("Below 20")
+
+}
+else 
+{
+    console.log("20 or higher")
+
+}
+for(i= 0;i< 5;i++)
+{
+    console.log("value of i :"+ i)
+}
+
+console.log('/a/b/c'.split('/'))
+c= 'hello' + '\n' + 'world'
+console.log(c)
